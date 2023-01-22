89b34f4c857d8aeeb568916cc3a4b2846a274b66
Adam Rao <raoadam87@gmail.com>
2023-01-22 22:53:32 +0300

    Define handle_client_connection function

--- server.py
+++ server.py
@@ -5,13 +5,25 @@ PORT: int = 2781
 HOST_IP: str = gethostbyname(gethostname()) # Get IP address of host computer name
 ENCODING_DECODING_FORMAT: str = "utf-8"
 
+COMMAND_SIZE: int = 2048 # Client and server can send and receive 2KB of data at a time
+
 # Create socket that accepts IPv4 or IPv6 addresses and receives and transmits streams of data
 server: socket = socket(AF_INET, SOCK_STREAM)
 
 server.bind((HOST_IP, PORT))
 
 def handle_client_connection(conn, addr):
-    pass
+    connected: bool = True
+
+    while connected:
+        client_command = conn.recv(COMMAND_SIZE).decode(ENCODING_DECODING_FORMAT)
+
+        if str(client_command).lower == "exit":
+            connected = False
+
+        conn.sendall(client_command) # send command to all clients connected to server
+
+    conn.close()
 
 while True:
     print(f"Staring server....")
