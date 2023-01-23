b3869499f59ff0dc7542a5cfbac0d14e16e6cfea
Adam Rao <raoadam87@gmail.com>
2023-01-23 19:03:03 +0300

    Set up placeholder message broadcast

--- server.py
+++ server.py
@@ -1,11 +1,15 @@
 from socket import gethostbyname, gethostname, socket, AF_INET, SOCK_STREAM
 import threading
 
+from linked_list import LinkedList
+
 PORT: int = 2781
 HOST_IP: str = gethostbyname(gethostname()) # Get IP address of host computer name
 ENCODING_DECODING_FORMAT: str = "utf-8"
 
 COMMAND_SIZE: int = 2048 # Client and server can send and receive a maximum of 2KB of data at a time
+
+client_list: LinkedList = LinkedList()
 
 # Create socket that accepts IPv4 addresses and receives and transmits streams of data
 server: socket = socket(AF_INET, SOCK_STREAM)
@@ -14,21 +18,26 @@ server.bind((HOST_IP, PORT))
 server.listen()
 print(f"Server listening on port: {PORT}")
 
-def handle_client_connection(conn):
+def handle_client_connection(conn: socket, client_name: str):
+
+    client_node: LinkedList.Node = LinkedList.Node(client_name, conn)
+    client_list.insert_node(client_node)
+
     while True:
         client_command = conn.recv(COMMAND_SIZE).decode(ENCODING_DECODING_FORMAT)
 
         if client_command and client_command == "exit":
             conn.send(client_command.encode(ENCODING_DECODING_FORMAT))
+            client_list.remove_node(client_name)
             break
 
         if client_command:
-            server.sendall(client_command.encode(ENCODING_DECODING_FORMAT))
-            print(client_command)
+            client_list.broadcast_messages(client_command.encode(ENCODING_DECODING_FORMAT))
 
     conn.close()
 
 while True:
     client_conn, _ = server.accept() 
-    client_thread = threading.Thread(target=handle_client_connection, args=(client_conn,))
+    client_name = client_conn.recv(COMMAND_SIZE).decode(ENCODING_DECODING_FORMAT) # Receive one time client name
+    client_thread = threading.Thread(target=handle_client_connection, args=(client_conn, client_name))
     client_thread.start()
